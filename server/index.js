f6c871eabcd04790a8f09f53213da01a010690b4
Toan Dao <toanddao@gmail.com>
2022-08-09 10:36:14 -0700

    set up gitignore

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000
+const router = require('./routes.js');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.use('/liftmeup', router);
 
 app.listen(port, () => {
   console.log(`Server listening at localhost:${port}!`);
